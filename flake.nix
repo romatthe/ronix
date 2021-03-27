daeeabff123931a4df6880ff10d3ccb7f3770cc7
Robin Mattheussen <Robin Mattheussen>
2021-03-27 15:10:09 +0100

    Added dependency on Home-Manager

--- flake.nix
+++ flake.nix
@@ -4,13 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/master";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, ... } @ inputs: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... } @ inputs: {
 
     nixosConfigurations = {
       
-      # Yokohama
+      # Yokohama, home desktop workhorse
       yokohama = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ (import ./machines/yokohama.nix) ];
