6feb82ee1e41ab5c810e8b6560a47ae3469f54e7
Robin Mattheussen <me@romatthe.dev>
2021-03-30 22:24:44 +0200

    Add spotify

--- modules/emacs/init.el
+++ modules/emacs/init.el
@@ -129,25 +129,26 @@
     "t" '(:ignore t :which-key "toggles")
     "tt" '(counsel-load-theme :which-key "choose theme")))
 
-(use-package evil
-  :init
-  (setq evil-want-integration t
-	evil-want-keybinding nil
-	evil-want-C-u-scroll t
-	evil-want-C-i-jump nil)
-  :config
-  (evil-mode 1)
-  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+; (use-package evil
+;  :init
+;  (setq evil-want-integration t
+;	evil-want-keybinding nil
+;	evil-want-C-u-scroll t
+;	evil-want-C-i-jump nil)
+;  :config
+;  (evil-mode 1)
+;  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+;  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
 
   ;; Use visual line motions even outside of visual-line-mode buffers
-  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+;  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+;  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
 
-  (evil-set-initial-state 'message-buffer-mode 'normal)
-  (evil-set-initial-state 'dashboard-mode 'normal))
+;  (evil-set-initial-state 'message-buffer-mode 'normal)
+;  (evil-set-initial-state 'dashboard-mode 'normal))
 
-(use-package evil-collection
-  :after evil
-  :config
-  (evil-collection-init))
+;(use-package evil-collection
+;  :after evil
+;  :config
+;  (evil-collection-init))
+
---
+++ modules/music.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+
+  home-manager.users.romatthe = {
+    home.packages = [
+      pkgs.spotify
+    ];
+  };   
+
+}
