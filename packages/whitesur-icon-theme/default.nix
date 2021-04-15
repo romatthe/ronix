92b35fd7e54037b851b86ec777ff276789cee880
Robin Mattheussen <me@romatthe.dev>
2021-04-16 00:45:30 +0200

    Add custom packages

---
+++ packages/whitesur-icon-theme/default.nix
@@ -0,0 +1,36 @@
+{ pkgs, stdenv, ... }:
+
+stdenv.mkDerivation rec {
+  name = "WhiteSur-icon-theme";
+  pname = "WhiteSur-icon-theme";
+  version = "2020-10-11";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "vinceliuice";
+    repo = pname;
+    rev = version;
+    sha256 = "1az7aa3p0knc9kjhk9bqpl9x230b4ygav4fb4pr3wccikxvy61h4";
+  };
+
+  buildInputs = with pkgs; [
+    sassc
+    optipng
+    inkscape
+    glib
+    libxml2
+    gtk3
+  ];
+
+  propagatedBuildInputs = with pkgs; [
+    numix-icon-theme-circle
+    gnome3.adwaita-icon-theme
+    hicolor-icon-theme
+  ];
+
+  postPatch = "patchShebangs .";
+
+  installPhase = ''
+    mkdir -p $out/share/icons
+    ./install.sh -d $out/share/icons
+  '';
+}
